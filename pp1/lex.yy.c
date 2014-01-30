40b702937dbbf8daeb80f0a03eccc53808be5c5f
Alfonso Kim <alfonsokim84@gmail.com>
2014-01-30 14:20:28 -0600

    ya casi parsea todos, creo que /********/ hace que falle

--- pp1/lex.yy.c
+++ pp1/lex.yy.c
@@ -443,8 +443,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	(yy_c_buf_p) = yy_cp;
 
 /* %% [4.0] data tables for the DFA and the user's section 1 definitions go here */
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -452,18 +452,19 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[11] =
+static yyconst flex_int16_t yy_accept[13] =
     {   0,
-        0,    0,    5,    4,    4,    4,    3,    2,    1,    0
+        0,    0,    4,    3,    3,    0,    1,    0,    0,    1,
+        2,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
     {   0,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    2,    1,    1,    1,    1,    3,    1,    1,    1,
+        1,    3,    1,    1,    1,    1,    4,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -489,33 +490,33 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int32_t yy_meta[4] =
+static yyconst flex_int32_t yy_meta[5] =
     {   0,
-        1,    1,    2
+        1,    2,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[12] =
+static yyconst flex_int16_t yy_base[16] =
     {   0,
-        0,    0,    7,    8,    0,    2,    8,    8,    8,    8,
-        4
+       10,    9,   12,   15,    0,    8,    0,    7,    5,    0,
+       15,   15,    4,    6,    0
     } ;
 
-static yyconst flex_int16_t yy_def[12] =
+static yyconst flex_int16_t yy_def[16] =
     {   0,
-       10,    1,   10,   10,   11,   10,   10,   10,   10,    0,
-       10
+       13,   13,   12,   12,   12,   14,   15,   14,   14,   15,
+       12,    0,   12,   12,   12
     } ;
 
-static yyconst flex_int16_t yy_nxt[12] =
+static yyconst flex_int16_t yy_nxt[20] =
     {   0,
-        4,    5,    6,    8,    9,    7,   10,    3,   10,   10,
-       10
+       10,   12,    6,    7,    4,    4,    8,    8,   11,    9,
+        9,   12,    5,    5,    3,   12,   12,   12,   12
     } ;
 
-static yyconst flex_int16_t yy_chk[12] =
+static yyconst flex_int16_t yy_chk[20] =
     {   0,
-        1,    1,    1,    6,    6,   11,    3,   10,   10,   10,
-       10
+       15,    0,    5,    5,   13,   13,   14,   14,    9,    8,
+        6,    3,    2,    1,   12,   12,   12,   12,   12
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -524,9 +525,9 @@ static char *yy_last_accepting_cpos;
 extern int yy_flex_debug;
 int yy_flex_debug = 1;
 
-static yyconst flex_int16_t yy_rule_linenum[4] =
+static yyconst flex_int16_t yy_rule_linenum[3] =
     {   0,
-       69,   70,   71
+       66,   67
     } ;
 
 /* The intent behind this definition is that it'll catch
@@ -591,7 +592,7 @@ static void DoBeforeEachAction();
 * as a simple substitution mechanism that allows for more readable
 * entries in the Rules section later. 
 */
-#line 595 "lex.yy.c"
+#line 596 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -833,13 +834,13 @@ YY_DECL
 	register int yy_act;
     
 /* %% [7.0] user's declarations go here */
-#line 64 "scanner.l"
+#line 61 "scanner.l"
 
  /* All patterns and actions should be placed between the start and stop
   * %% markers which delimit the Rules section. 
   */ 
 
-#line 843 "lex.yy.c"
+#line 844 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -902,13 +903,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 11 )
+				if ( yy_current_state >= 13 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 8 );
+		while ( yy_base[yy_current_state] != 15 );
 
 yy_find_action:
 /* %% [10.0] code to find the action number goes here */
@@ -931,13 +932,13 @@ do_action:	/* This label is used only to access EOF actions. */
 			{
 			if ( yy_act == 0 )
 				fprintf( stderr, "--scanner backing up\n" );
-			else if ( yy_act < 4 )
+			else if ( yy_act < 3 )
 				fprintf( stderr, "--accepting rule at line %ld (\"%s\")\n",
 				         (long)yy_rule_linenum[yy_act], yytext );
-			else if ( yy_act == 4 )
+			else if ( yy_act == 3 )
 				fprintf( stderr, "--accepting default rule (\"%s\")\n",
 				         yytext );
-			else if ( yy_act == 5 )
+			else if ( yy_act == 4 )
 				fprintf( stderr, "--(end of buffer or a NUL)\n" );
 			else
 				fprintf( stderr, "--EOF (start condition %d)\n", YY_START );
@@ -955,25 +956,21 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 69 "scanner.l"
+#line 66 "scanner.l"
 {printf("doble-diag");}
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 70 "scanner.l"
-{printf("inicio_comentario_multilinea");}
+#line 67 "scanner.l"
+{printf("comentario_multilinea");}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 71 "scanner.l"
-{printf("fin_comentario_multilinea");}
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 73 "scanner.l"
+#line 70 "scanner.l"
 ECHO;
 	YY_BREAK
-#line 977 "lex.yy.c"
+#line 974 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1286,7 +1283,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 11 )
+			if ( yy_current_state >= 13 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1319,11 +1316,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 11 )
+		if ( yy_current_state >= 13 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 10);
+	yy_is_jam = (yy_current_state == 12);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -2097,7 +2094,7 @@ void yyfree (void * ptr )
 
 /* %ok-for-header */
 
-#line 73 "scanner.l"
+#line 70 "scanner.l"
 
 
 /* The closing %% above marks the end of the Rules section and the beginning
