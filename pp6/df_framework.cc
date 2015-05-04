69bc7d6b76e269403776dcf0f5a7972d6c541ae6
Alfonso Kim <alfonsokim84@gmail.com>
2015-05-03 19:54:58 -0500

    greedo; revision de codigo

--- pp6/df_framework.cc
+++ pp6/df_framework.cc
@@ -30,8 +30,8 @@ void DFFrameworkType::InitFramework(std::list<Instruction*>& todo) {
 //////////////////////////////////////////////////////////////////
 
 DFFrameworkType::DFFrameworkType(CFGBaseType* cfg, DF_DIRECTION_TYPE dir) {
-  controlFlowGraph = cfg;
-  direction = dir;
+    controlFlowGraph = cfg;
+    direction = dir;
 }
     
 
@@ -42,40 +42,40 @@ DFFrameworkType::DFFrameworkType(CFGBaseType* cfg, DF_DIRECTION_TYPE dir) {
 
 
 void DFFrameworkType::RunFramework() {
-  // if backward we need to reverse the cfg
-  if (direction == DF_BACKWARD) {
-    controlFlowGraph->ReverseCFG();
-  }
-
-  std::list<Instruction*> todo;
-  // next initialize the nodes
-  InitFramework(todo);
-
-  // start the iterative data flow. All the nodes
-  // will be processed until a fixed point is reached 
-  // (i.e. no change)  
-  while (todo.size() > 0) {
-    Instruction* node = todo.front();
-    todo.pop_front();
-    controlFlowGraph->Status(node,DF_NOT_IN_TODO);
-    // first apply the meet operator
-    controlFlowGraph->ApplyMeetOperator(node);
-    // next apply the transfer function
-    bool changed = controlFlowGraph->ApplyTransferFunction(node);
-    // if applying the transfer function changed the data sets
-    // the out edges of this node need to be added to the todo
-    // list since they might also change.
-    if (changed) {
-      std::vector<Instruction*>& next_nodes = controlFlowGraph->GetOutEdges(node);
-      for (int cnt=0; cnt < next_nodes.size(); ++cnt) {
-	    Instruction* next_node = next_nodes[cnt];
-	    // only need to add to todo list if not there already
-	    if (controlFlowGraph->Status(next_node) != DF_IN_TODO) {
-	      controlFlowGraph->Status(next_node, DF_IN_TODO);
-	      todo.push_back(next_node);
-	    }
-      }
+    // if backward we need to reverse the cfg
+    if (direction == DF_BACKWARD) {
+        controlFlowGraph->ReverseCFG();
     }
-  }
+
+    std::list<Instruction*> todo;
+    // next initialize the nodes
+    InitFramework(todo);
+
+    // start the iterative data flow. All the nodes
+    // will be processed until a fixed point is reached 
+    // (i.e. no change)  
+    while (todo.size() > 0) {
+        Instruction* node = todo.front();
+        todo.pop_front();
+        controlFlowGraph->Status(node,DF_NOT_IN_TODO);
+        // first apply the meet operator
+        controlFlowGraph->ApplyMeetOperator(node);
+        // next apply the transfer function
+        bool changed = controlFlowGraph->ApplyTransferFunction(node);
+        // if applying the transfer function changed the data sets
+        // the out edges of this node need to be added to the todo
+        // list since they might also change.
+        if (changed) {
+            std::vector<Instruction*>& next_nodes = controlFlowGraph->GetOutEdges(node);
+            for (int cnt=0; cnt < next_nodes.size(); ++cnt) {
+               Instruction* next_node = next_nodes[cnt];
+               // only need to add to todo list if not there already
+               if (controlFlowGraph->Status(next_node) != DF_IN_TODO) {
+                    controlFlowGraph->Status(next_node, DF_IN_TODO);
+                    todo.push_back(next_node);
+                }
+            }
+        }
+    }
 }
 
